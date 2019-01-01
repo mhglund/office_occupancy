e996b130481c4056a8e95cb95ea3b4182674d49b
mhglund <a.m.hglund@gmail.com>
2019-01-01 19:49:04 +0100

    Read 1000 lines of Yanzi data, and parse the different time formats

--- test_yanzi.py
+++ test_yanzi.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 import pandas as pd
 import numpy as np
+import arrow
+from dateutil import tz
+
 
 # Läs in filen
-df = pd.read_csv('yanzi_250l.csv', sep=';', encoding = "utf-8")
+df = pd.read_csv('yanzi_1000l.csv', sep=';', encoding = "utf-8")
 sensorlist = pd.read_csv('sensor_list.csv', sep=';', encoding = "utf-8")
 
 # Plocka ut 10 första raderna för att inte skriva ut så mkt
@@ -24,11 +27,34 @@ print('--------------------------')
 df_trimmed = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
 sensorlist_trimmed = sensorlist.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
 
+
 # Konvertera datumformatet
-df_trimmed['time'] = pd.to_datetime(df_trimmed['time'], unit='ms')
+def convert_time(text):
+    try:
+        return arrow.get(text)
+    except:
+        try:
+             return arrow.get(text, 'YYYY-M-D HH:mm:ss')
+        except:
+            try:
+               return arrow.get(text[:10]+'.'+text[-3:])
+            except:
+                return arrow.get(text[:-4], 'ddd, D MMMM YYYY HH:mm:ss')
+
+def convert_timeformat(text):
+    temp = convert_time(text)
+    temp.tzinfo = tz.gettz('GMT')
+    return temp.datetime
+
+df_trimmed['time'] = pd.to_datetime(df_trimmed.loc[:,'time'].transform(convert_timeformat))
+
+print('25 slumpade rader:')
+print(df_trimmed.sample(25))
+print('--------------------------')
 print('INFO m. nytt datumformat:')
 print(df_trimmed.info())
 print('--------------------------')
+
 
 # Plocka ut en viss sensor (rader med ett visst kolumnvärde)
 en_sensor = df_trimmed.loc[df_trimmed['sensorId'] == 'EUI64-0080E10300045738-4-Motion']
@@ -48,12 +74,13 @@ df_movement = df_vissa_kolumner.join(pd.DataFrame({'movement': haschanged}))
 
 #df_vissa_kolumner.join(pd.DataFrame({'movement': df_vissa_kolumner.groupby('sensorId')['value'].diff().transform(lambda x: x > 0)}))
 
-print('SENSOR: EUI64-0080E10300045738-4-Motion')
 print('Med movement (true/false):')
 print(df_movement.head(100))
 print('--------------------------')
+print('SENSOR: EUI64-0080E10300045738-4-Motion')
 print(en_sensor)
 print('--------------------------')
+
 
 # Plocka ut rader med tomma värden
 tomma = df_trimmed.isnull().values.any()
@@ -61,6 +88,7 @@ tomma = df_trimmed.isnull().values.any()
 print('SAKNAR EJ VÄRDE:')
 print(tomma)
 print('--------------------------')
+
 
 # Skapar en ny DF(kopia) för att kunna lägga till rum
 newDF = pd.DataFrame(df_trimmed)
